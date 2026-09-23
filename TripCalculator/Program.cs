11df21d4e7a0748021567cbe0abdbef4876be044
Justin <williamsjp2@etsu.edu>
2026-09-23 14:19:46 -0400

    Add pizza party calculator

--- TripCalculator/Program.cs
+++ TripCalculator/Program.cs
@@ -24,3 +24,25 @@ double fuelCost = gallonsNeeded * pricePerGallon;
 // Output
 System.Console.WriteLine("\nGallons needed: " + gallonsNeeded.ToString("F2"));
 System.Console.WriteLine("Fuel Cost: " + fuelCost.ToString("C"));
+
+// Pizza Party input
+System.Console.WriteLine("\n=== Part 2: Pizza Party ===");
+System.Console.Write("How many people are going? ");
+int peopleGoing = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("How many pizzas? ");
+int pizzas = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("What is the price per pizza? ");
+double pizzaPrice = Convert.ToDouble(Console.ReadLine());
+
+// How many slices per pizza, and how many slices total that makes, and price
+const int slicesperPizza = 8;
+double totalSlices = slicesperPizza * pizzas;
+double slicesPerPerson = totalSlices / peopleGoing;
+double pizzaCost = pizzas * pizzaPrice;
+
+// Pizzas Output
+System.Console.WriteLine("\nTotal slices: " + totalSlices);
+System.Console.WriteLine("Slices per person: " + slicesPerPerson.ToString("F1"));
+System.Console.WriteLine("Pizza cost: " + pizzaCost.ToString("C"));
